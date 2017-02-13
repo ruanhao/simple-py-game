83909001d2ac4d16d64bc729f92e71f49c26898d
Hao Ruan <haoru@cisco.com>
2017-02-13 15:21:06 +0800

    refactor update_bullets

--- game_functions.py
+++ game_functions.py
@@ -71,6 +71,9 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     ## first True: to remove bullet
     ## second True: to remove alien
     pygame.sprite.groupcollide(bullets, aliens, True, True)
