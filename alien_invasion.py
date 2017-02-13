cc0809c65cd96b33bb281dc784929e6890897e3f
Hao Ruan <haoru@cisco.com>
2017-02-13 13:38:53 +0800

    add Bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,10 +15,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -9,3 +9,7 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 8
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
