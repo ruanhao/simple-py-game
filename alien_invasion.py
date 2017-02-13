ffa1db614d79d695b0c866fe0135a5b6a0e1d31e
Hao Ruan <haoru@cisco.com>
2017-02-13 11:09:32 +0800

    add Settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,19 +4,20 @@
 import pygame
 import sys
 
+from settings import Settings
+
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    bg_color = (230, 230, 230)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Settings():
+    """Save all settings"""
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
