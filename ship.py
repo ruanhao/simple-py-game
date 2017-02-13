d59cde8db2100f2b56d657bf03e87c7f2cf85341
Hao Ruan <haoru@cisco.com>
2017-02-13 13:00:01 +0800

    make movement fast

--- ship.py
+++ ship.py
@@ -5,8 +5,9 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
+        self.ai_settings = ai_settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -14,6 +15,8 @@ class Ship():
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
 
         self.moving_right = False
         self.moving_left = False
@@ -23,6 +26,7 @@ class Ship():
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
