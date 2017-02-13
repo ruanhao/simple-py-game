b6f0cab95cfda3d65c62786d97690250842c644a
Hao Ruan <haoru@cisco.com>
2017-02-13 10:59:13 +0800

    create screen

---
+++ alien_invasion.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pygame
+import sys
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+run_game()
